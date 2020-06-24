d8045ec567ac187d0256e0fb7d85666ee43bfc0b
Inspectiver <33937835+Inspectiver@users.noreply.github.com>
2020-06-24 16:29:21 +0100

    Added ban command

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 
@@ -13,7 +14,45 @@ namespace TutorialBot.Modules
         [Command("ping")]
         public async Task Ping()
         {
-            await ReplyAsync("Pong");
+            await ReplyAsync("pong");
+        }
+        
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage ="You don't have the permission ``ban_member``!")]
+        public async Task BanMember(IGuildUser user = null, [Remainder] string reason = null)
+        {
+            if (user == null)
+            {
+                await ReplyAsync("Please specify a user!"); 
+                return;
+            }
+            if (reason == null) reason = "Not specified";
+
+            await Context.Guild.AddBanAsync(user, 1, reason);
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention} was banned\n**Reason** {reason}")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText("User Ban Log")
+                    .WithIconUrl("https://i.imgur.com/6Bi17B3.png");
+                });
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+
+            ITextChannel logChannel = Context.Client.GetChannel(642698444431032330) as ITextChannel;
+            var EmbedBuilderLog = new EmbedBuilder()
+                .WithDescription($"{user.Mention} was banned\n**Reason** {reason}\n**Moderator** {Context.User.Mention}")
+                .WithFooter(footer =>
+                {
+                    footer
+                    .WithText("User Ban Log")
+                    .WithIconUrl("https://i.imgur.com/6Bi17B3.png");
+                });
+            Embed embedLog = EmbedBuilderLog.Build();
+            await logChannel.SendMessageAsync(embed: embedLog);
+
         }
     }
 }
